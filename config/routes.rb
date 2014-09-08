b4c95307d7003fc9165df37cdca38c83bcfae5bb
Steve Madere <steve@stevemadere.com>
2014-09-08 08:40:20 -0500

    changed routing directive from match to get to comply with Rails 4.x requirements

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   @controller = ApplicationController.subclasses.find{|c| c.ancestors.include?(HoboOmniauth::Controller)}
   if @controller
-    match '/auth/:provider/callback' => "#{@controller.controller_name}#omniauth_callback"
-    match '/auth/failure' => "#{@controller.controller_name}#omniauth_callback"
+    get '/auth/:provider/callback' => "#{@controller.controller_name}#omniauth_callback"
+    get '/auth/failure' => "#{@controller.controller_name}#omniauth_callback"
   end
 end
